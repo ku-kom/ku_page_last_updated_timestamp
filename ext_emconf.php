09a69b74206c50a9c071b8a239c8e8db96999f6e
nanna-dk <nanna@e-nanna.dk>
2022-11-23 15:52:44 +0100

    Init

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the package ku_phonebook.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ * Sep 2022 Nanna Ellegaard, University of Copenhagen.
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'KU page last updated timestamp',
+    'description' => 'When a content element is added or edited, the latest modified date is added for its corresponding page.',
+    'category' => 'templates',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Nanna Ellegaard',
+    'author_email' => 'nel@adm.ku.dk',
+    'author_company' => 'University of Copenhagen',
+    'version' => '1.0.0',
+];
---
+++ ext_tables.sql
@@ -0,0 +1,6 @@
+#
+# Add SQL definition of database tables
+#
+CREATE TABLE pages (
+    tx_kulastpageupdate_timestamp int(11) default '0' NOT NULL
+);
